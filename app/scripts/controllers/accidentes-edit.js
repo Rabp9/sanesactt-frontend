7f0fd0abcc006bc213254389c47fbac80288a9b3
tmtrbocanegra <tmtrbocanegra@DESKTOP-PTTSRKA.TMT.LOCAL>
2017-04-26 11:09:20 -0500

    show ubicaciones select edit accidentes

--- app/scripts/controllers/accidentes-edit.js
+++ app/scripts/controllers/accidentes-edit.js
@@ -8,10 +8,25 @@
  * Controller of the sanesacttFrontendApp
  */
 angular.module('sanesacttFrontendApp')
-  .controller('AccidentesEditCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('AccidentesEditCtrl', function ($scope, accidente, $uibModalInstance, AccidentesService, UbicacionesService) {
+    $scope.accidente = $.extend(true, {}, accidente)
+    
+    UbicacionesService.get(function(data) {
+        $scope.ubicaciones = data.ubicaciones;
+    })
+    
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.savePage = function(page, boton) {
+        $('#' + boton).addClass('disabled');
+        $('#' + boton).prop('disabled', true);
+        
+        AccidentesService.save(page, function(data) {
+            $('#' + boton).removeClass('disabled');
+            $('#' + boton).prop('disabled', false);
+            $uibModalInstance.close(data);
+        });
+    };
+});
