56a89f6b286735cdce7df03e894abc36fc15a8ab
tmtrbocanegra <rabp_91@hotmail.com>
2018-05-04 08:52:11 -0500

    62. Crear Formulario "Editar rol" 63. Crear Controller "Administrar roles"

---
+++ app/scripts/controllers/roles-edit.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sanesacttFrontendApp.controller:RolesEditCtrl
+ * @description
+ * # RolesEditCtrl
+ * Controller of the sanesacttFrontendApp
+ */
+angular.module('sanesacttFrontendApp')
+  .controller('RolesEditCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- app/scripts/controllers/roles.js
+++ app/scripts/controllers/roles.js
@@ -8,10 +8,53 @@
  * Controller of the sanesacttFrontendApp
  */
 angular.module('sanesacttFrontendApp')
-  .controller('RolesCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('RolesCtrl', function ($scope, RolesService, $uibModal, $utilsViewService) {
+    $scope.loading = true;
+    
+    function getRoles() {    
+        $scope.loading = true;
+        RolesService.get(function(data) {
+            $scope.roles = data.roles;
+            $scope.loading = false;
+        });
+    }
+    
+    getRoles();
+    
+    $scope.showRolesAdd = function(event) {
+        $utilsViewService.disable(event.currentTarget);
+        
+        var modalInstanceAdd = $uibModal.open({
+            templateUrl: 'views/roles-add.html',
+            controller: 'RolesAddCtrl',
+            backdrop: false
+        });
+        
+        $utilsViewService.enable(event.currentTarget);
+        modalInstanceAdd.result.then(function (data) {
+            getRoles();
+            $scope.message = data;
+        });
+    };
+    
+    $scope.showRolesEdit = function(rol_id, event) {
+        $utilsViewService.disable(event.currentTarget);
+        
+        var modalInstanceEdit = $uibModal.open({
+            templateUrl: 'views/roles-edit.html',
+            controller: 'RolesEditCtrl',
+            backdrop: false,
+            resolve: {
+                rol_id: function() {
+                    return rol_id;
+                }
+            }
+        });
+        
+        $utilsViewService.enable(event.currentTarget);
+        modalInstanceEdit.result.then(function (data) {
+            getRoles();
+            $scope.message = data;
+        });
+    };
+});
